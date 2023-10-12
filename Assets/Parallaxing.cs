44958f2621eda6aae96faf2682f7f263cc3c15f9
HighKingOfTheSevenSeasSinbad <145032832+HighKingOfTheSevenSeasSinbad@users.noreply.github.com>
2023-10-12 15:37:55 +0100

    gxfcvg badz\sf

---
+++ Assets/Parallaxing.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    GameObject player;
+    Renderer rend;
+
+    float playerStartPos;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Mr.Blank");
+        rend = GetComponent<Renderer>();
+        playerStartPos = player.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = (player.transform.position.x - playerStartPos) * speed;
+
+
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
+
+    }
+}
+
+--
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
